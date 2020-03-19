9e16042db83eefb2bae669812d561cb4c8b31eb7
Gustavo Guntzel <gustavo.g.s01@hotmail.com>
2020-03-18 21:47:00 -0700

    added basic file structure, HTML and CSS

---
+++ index.js
@@ -0,0 +1,7 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const restart = document.getElementById('restart');
+const expand = document.getElementById('expand');
+const volume = document.getElementById('volume');
+
+console.log(video, play, restart, expand, volume);
